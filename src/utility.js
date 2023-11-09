4691a8e96608e4ef48e44c19ee67a528670095b6
Roberto Tencic <roberto.tencic@outlook.com>
2023-11-09 20:40:08 +0100

    Update

--- src/utility.js
+++ src/utility.js
@@ -16,11 +16,30 @@ class Utility {
         return clampNum;
     }
 
-    static saveToLocalStorage(name, value) {
-        localStorage.setItem(name, value);
+    static saveToLocalStorage(id, value) {
+        for (let i = 0; i < localStorage.length; i++) {
+            if (localStorage.key(i) === id) {
+                console.warn('Overwriting item "' + id + '" with a new value.');
+            }
+        }
+        localStorage.setItem(id, value);
     }
 
-    static readFromLocalStorage(name) {
-        return localStorage.getItem(name);
+    static readFromLocalStorage(id) {
+        let value = null;
+        try {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) === id) {
+                    value = localStorage.getItem(localStorage.key(i));
+                    break;
+                }
+            }
+            if (value === null) {
+                throw 'Item with key "' + id + '" was not found in local storage.';
+            }
+        } catch (err) {
+            console.console.error(err);
+        }
+        return value;
     }
 }
